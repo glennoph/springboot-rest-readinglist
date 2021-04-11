0f4bc15072af1ba54f1ae3f7ffbf394d2bc631f1
glenn opdycke-hansen <glennoph@gmail.com>
2021-04-11 13:21:49 -0500

    init

---
+++ src/test/java/go/springboot/restsbia/readinglist/ReadinglistApplicationTests.java
@@ -0,0 +1,13 @@
+package go.springboot.restsbia.readinglist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadinglistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
